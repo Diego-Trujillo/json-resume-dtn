ace354ffd7b5f64cef9745634c677d1c4bd5c015
Diego Trujillo <diego.tn94@gmail.com>
2018-03-20 07:55:38 +0000

    Added language integration, finished experience section. Made light CV alterations.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,42 @@
+// =========================================== DEPENDENCIES ====================================
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// =========================================== CONFIG ==========================================
+// ---- Read and store all available language structures
+var languagesAvailable = JSON.parse(fs.readFileSync(__dirname + '/../schema/config.json', 'utf8')).languages;
+var schema = {};
+
+languagesAvailable.forEach(function(language){
+  schema[language] = {
+    pageElements: JSON.parse(fs.readFileSync(__dirname + '/../schema/page-elements/' + language + '.json', 'utf8')),
+    portfolio: JSON.parse(fs.readFileSync(__dirname + '/../schema/portfolio/' + language + '.json', 'utf8')),
+    resume: JSON.parse(fs.readFileSync(__dirname + '/../schema/resume/' + language + '.json', 'utf8'))
+  }
 });
 
+
+// =========================================== INDEX ===========================================
+router.get('/', function(req, res, next) {
+  res.redirect('/resume/es');
+});
+
+// =========================================== RESUME ==========================================
+router.get('/resume/:lang', function(req, res, next){
+  var lang = req.params.lang;
+  if(!languagesAvailable.includes(lang)){
+    res.send('we dont have it goy.');
+  }
+  else{
+    console.log(schema[lang])
+    res.render('resume.ejs', { schema: schema[lang], languages: { current: lang, available: languagesAvailable } });
+  }
+});
+
+// =========================================== PORTFOLIO =======================================
+
+// =========================================== EXPORT ===========================================
 module.exports = router;
+
+// =========================================== UTILITY ===========================================
